4dfe4a013c41a69785e8c4405099e1ba72fd3e0e
Glavo <zjx001202@gmail.com>
2024-06-08 21:06:53 +0800

    Create create-post.sh

---
+++ scripts/create-post.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd "$(dirname $0)/.."
+
+_date=$(date '+%F')
+_time=$(date '+%F %H:%M:%S')
+_file_name="./_drafts/$_date-$1.md"
+
+echo "---
+title: '$1'
+date: $_time
+tags:
+  - 
+categories: blog
+description:
+---
+" > $_file_name
